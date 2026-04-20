34de53f61f077e5bc564fa5628759168d48ec76f
Andrey Marshak <etotot@etotot.xyz>
2026-04-20 14:38:08 +0300

    Rename AxPermissionManager to AXPermissionManager

--- Plakaki/Sources/Permissions/AxPermissionManager.swift
+++ Plakaki/Sources/Permissions/AxPermissionManager.swift
@@ -1,5 +1,5 @@
 //
-//  A11yPermissionManager.swift
+//  AXPermissionManager.swift
 //  Plakaki
 //
 //  Created by Andrey Marshak on 18/04/2026.
@@ -9,14 +9,14 @@
 import Dependencies
 import Foundation
 
-struct AxPermissionManager: Sendable {
+struct AXPermissionManager {
     var checkAxStatus: @Sendable (_ triggerPrompt: Bool) -> Bool
     var openSystemSettings: @Sendable () -> Void
 }
 
-extension AxPermissionManager: DependencyKey {
+extension AXPermissionManager: DependencyKey {
     static var liveValue: Self {
-        return Self(
+        Self(
             checkAxStatus: { (triggerPrompt: Bool) -> Bool in
                 let promptKey: String = kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String
                 let options: CFDictionary = [promptKey: triggerPrompt] as CFDictionary
@@ -32,8 +32,8 @@ extension AxPermissionManager: DependencyKey {
 }
 
 extension DependencyValues {
-    var axPermissionManager: AxPermissionManager {
-        get { self[AxPermissionManager.self] }
-        set { self[AxPermissionManager.self] = newValue }
+    var axPermissionManager: AXPermissionManager {
+        get { self[AXPermissionManager.self] }
+        set { self[AXPermissionManager.self] = newValue }
     }
 }
